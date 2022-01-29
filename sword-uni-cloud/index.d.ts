65e5fa60332a054d0cd602e6a9bab5b93969a9f1
seho <1018715564@qq.com>
2022-01-29 15:44:57 +0800

    🐞 fix: v1.1.3

--- sword-uni-cloud/index.d.ts
+++ sword-uni-cloud/index.d.ts
@@ -1,4 +1,4 @@
-export declare interface CloudContext {
+declare interface CloudContext {
   // 客户端ip信息
   CLIENTIP: string;
   // 客户端user-agent
@@ -12,17 +12,17 @@ export declare interface CloudContext {
 }
 
 // 调用explain框架的event，普通云函数的event就是传递的参数
-export declare interface ExplainCloudEvent {
+declare interface ExplainCloudEvent {
   service: string;
   action: string;
   data: unknown;
   headers: Record<string, string>;
 }
 
-export declare type CloudData = { event: ExplainCloudEvent; context: CloudContext; explain: unknown };
+declare type CloudData = { event: ExplainCloudEvent; context: CloudContext; explain: unknown };
 // 请求方法类型
-export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE';
-export declare type CloudRouter = {
+type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE';
+declare type CloudRouter = {
   route: `api/${string}`;
   service: string;
   routes: {
