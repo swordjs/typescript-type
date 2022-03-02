7796577c6e1184f32fd151ac37726ec2ce9a4444
seho <1018715564@qq.com>
2022-03-02 23:00:18 +0800

    🎈 perf: 完善Use类型

--- sword-backend-framework/index.d.ts
+++ sword-backend-framework/index.d.ts
@@ -1,8 +1,10 @@
 export interface ContextData {
-  query: Record<string, string>;
-  params: Record<string, unknown>;
+  query?: Record<string, string>;
+  params?: Record<string, unknown>;
 }
 export interface HttpContext<T extends ContextData = ContextData> {
+  key: string; // context的key由api构造
+  proto: Record<string, unknown>;
   query: T["query"];
   params: T["params"];
 }
@@ -26,10 +28,29 @@ export type HttpInstructReturn = {
 export type HttpApi = <C extends ContextData>(
   instruct: HttpInstructReturn | HttpInstructReturn[],
   handler: (ctx: HttpContext<C>) => void
-) => {
+) => HttpApiReturn<C>;
+
+export type HttpApiReturn<C extends ContextData> = {
   instruct: {
     method: HttpInstructMethod | HttpInstructMethod[];
     path?: string;
   };
+  validateProto: Use["ValidateProto"];
   handler: (ctx: HttpContext<C>) => void;
 };
+
+export interface Use {
+  ValidateProto: (
+    key: string,
+    data: Record<string, unknown>,
+    schema: Record<string, unknown>
+  ) =>
+    | {
+        isSucc: true;
+        errMsg?: undefined;
+      }
+    | {
+        isSucc: false;
+        errMsg: string;
+      };
+}
